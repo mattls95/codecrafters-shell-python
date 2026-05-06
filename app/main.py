7a8e486cd02cbe632dcb49b92154620e558413f4
mattls <mattias.swenne@gmail.com>
2026-05-06 21:40:16 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,6 +6,11 @@ def main():
     while True:
         sys.stdout.write("$ ")
         user_cmd = input()
+
+        match user_cmd:
+            case 'exit':
+                break
+
         print(f"{user_cmd}: command not found")
 
 
