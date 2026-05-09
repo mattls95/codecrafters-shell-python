3d741645b6360df760809a3c6659aad93cecba82
mattls <mattias.swenne@gmail.com>
2026-05-09 18:49:30 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,6 +13,8 @@ def main():
                 type_arg = user_cmd_arg[1]
                 if type_arg in ('echo', 'exit', 'type'):
                     print(f"{type_arg} is a shell builtin")
+                else:
+                    print(f"{type_arg}: command not found")
             case 'echo':
                 print(" ".join(user_cmd_arg[1:]))
             case 'exit':
